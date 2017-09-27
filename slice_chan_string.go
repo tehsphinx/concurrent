238c86c3874e9915d6888ca628446cd593b8547b
Gabriel Nelle <nelle@raceresult.com>
2017-09-27 10:58:57 +0200

    add concurrent channel list of strings

---
+++ slice_chan_string.go
@@ -0,0 +1,122 @@
+package concurrent
+
+import (
+	"log"
+)
+
+type actionSliceChanString int
+
+const (
+	actionAddChanString actionSliceChanString = iota
+	actionRemoveChanString
+	actionRemoveAllChanString
+	actionSendChanString
+	actionLenChanString
+)
+
+type cmdSliceChanString struct {
+	action  actionSliceChanString
+	ch      chan string
+	chOk    chan bool
+	chLen   chan int
+	chRange chan chan string
+	msg     string
+}
+
+// NewSliceChanString creates a new concurrent slice of bytes
+func NewSliceChanString() *SliceChanString {
+	s := &SliceChanString{
+		slice: []chan string{},
+		chCmd: make(chan cmdSliceChanString, 5),
+	}
+	return s.run()
+}
+
+// SliceChanString implements a cuncurrent slice of bytes
+type SliceChanString struct {
+	slice []chan string
+	chCmd chan cmdSliceChanString
+}
+
+// Add appends a channel to the slice
+func (s *SliceChanString) Add(ch chan string) {
+	s.chCmd <- cmdSliceChanString{
+		action: actionAddChanString,
+		ch:     ch,
+	}
+}
+
+// Remove removes a channel from the slice
+func (s *SliceChanString) Remove(ch chan string) bool {
+	chOk := make(chan bool)
+	s.chCmd <- cmdSliceChanString{
+		action: actionRemoveChanString,
+		ch:     ch,
+		chOk:   chOk,
+	}
+	return <-chOk
+}
+
+func (s *SliceChanString) RemoveAll() {
+	s.chCmd <- cmdSliceChanString{
+		action: actionRemoveAllChanString,
+	}
+}
+
+func (s *SliceChanString) Send(msg string) {
+	s.chCmd <- cmdSliceChanString{
+		action: actionSendChanString,
+		msg:    msg,
+	}
+}
+
+func (s *SliceChanString) Len() int {
+	chLen := make(chan int)
+	s.chCmd <- cmdSliceChanString{
+		action: actionLenChanString,
+		chLen:  chLen,
+	}
+	return <-chLen
+}
+
+func (s *SliceChanString) run() *SliceChanString {
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Println("concurrent.(SliceChanString).run panicked:", r)
+			}
+
+			s.run()
+		}()
+
+		for c := range s.chCmd {
+			switch c.action {
+			case actionAddChanString:
+				s.slice = append(s.slice, c.ch)
+			case actionRemoveChanString:
+				found := false
+				for i, ch := range s.slice {
+					if ch == c.ch {
+						s.slice = append(s.slice[:i], s.slice[i+1:]...)
+						close(ch)
+						found = true
+					}
+				}
+				c.chOk <- found
+			case actionRemoveAllChanString:
+				for _, ch := range s.slice {
+					close(ch)
+				}
+				s.slice = []chan string{}
+			case actionSendChanString:
+				for _, ch := range s.slice {
+					ch <- c.msg
+				}
+			case actionLenChanString:
+				c.chLen <- len(s.slice)
+			}
+		}
+	}()
+
+	return s
+}
