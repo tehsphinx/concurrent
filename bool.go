8d43560272ade610c9ba962072c6e469328b7904
Gabriel Nelle <nelle@raceresult.com>
2018-11-28 19:26:33 +0100

    cleanup atomic bool

--- bool.go
+++ bool.go
@@ -1,39 +1,30 @@
 package concurrent
 
 import (
-	"sync"
 	"sync/atomic"
 )
 
 // NewBool creates a new concurrent bool
 func NewBool() *Bool {
-	return &Bool{
-		events: []chan bool{},
-	}
+	return new(Bool)
 }
 
 // Bool implements a cuncurrent bool
-type Bool struct {
-	bool      int32
-	boolMutex sync.RWMutex
-	events    []chan bool
-}
+type Bool int32
 
 // Set sets the bool to given value and returns if it changed or not. This
 // can be used in race cases where a value could change inbetween the 'if'
 // and setting the new value, making sure two routines executing simultaneously
 // will not both get the same result checking the bool.
 func (s *Bool) Set(b bool) (ok bool) {
-	var o, n int32
+	var o int32
 	if !b {
 		o = 1
-	} else {
-		n = 1
 	}
-	return atomic.CompareAndSwapInt32((*int32)(&s.bool), o, n)
+	return atomic.CompareAndSwapInt32((*int32)(s), o, 1-o)
 }
 
 // Get gets the bool value
 func (s *Bool) Get() bool {
-	return atomic.LoadInt32((*int32)(&s.bool)) == 1
+	return atomic.LoadInt32((*int32)(s)) == 1
 }
